13018ef6ec4e21bd4b45d3b668ddc147af58f229
debsmalheiro <malheirodev@gmail.com>
2021-11-22 20:54:29 -0300

    feat: Adicionando CSS Variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,18 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-192.png" />
-        <meta name="description" content="A simple project to work with TypeScript, React, NextJS and Styled Components" />
+        <meta
+          name="description"
+          content="A simple project to work with TypeScript, React, NextJS and Styled Components"
+        />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
       </Head>
       <GlobalStyles />
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </>
   )
 }
