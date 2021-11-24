cad2e7f53b238a41e4f884135601dd1674dc3892
debsmalheiro <malheirodev@gmail.com>
2021-11-24 19:08:38 -0300

    Criando página About (rota simples e estático)

---
+++ src/templates/About/index.tsx
@@ -0,0 +1,22 @@
+import * as S from './styles'
+
+import LinkWrapper from 'components/LinkWrapper'
+
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+
+const AboutTemplate = () => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} aria-label="Close" />
+    </LinkWrapper>
+
+    <S.Heading>
+      My Trips
+    </S.Heading>
+    <S.Body>
+      <p>Texto</p>
+    </S.Body>
+  </S.Content>
+)
+
+export default AboutTemplate
