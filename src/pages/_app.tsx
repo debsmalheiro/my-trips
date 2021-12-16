4aa01d5c4eefdb9abdd7110d6b72e89823ad503c
debsmalheiro <malheirodev@gmail.com>
2021-12-15 22:34:36 -0300

    Adicionando um loader entre as páginas - NProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import NextNProgress from 'nextjs-progressbar'
+
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
@@ -24,6 +26,12 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#06092B" />
       </Head>
       <GlobalStyles />
+      <NextNProgress
+        color="#f231a5"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
       <Component {...pageProps} />
     </>
   )
