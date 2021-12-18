2ea6980ac2d2ee8ad5dc072d1f7e060e2f4eb22b
debsmalheiro <malheirodev@gmail.com>
2021-12-18 16:27:04 -0300

    feat: revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
-import HomeTemplate from "templates/Home";
-import { MapProps } from "components/Map";
+import HomeTemplate from 'templates/Home'
+import { MapProps } from 'components/Map'
 
-import client from "graphql/client";
-import { GET_PLACES } from "graphql/queries";
-import { GetPlacesQuery } from "graphql/generated/graphql";
+import client from 'graphql/client'
+import { GET_PLACES } from 'graphql/queries'
+import { GetPlacesQuery } from 'graphql/generated/graphql'
 
-export default function Home({places}: MapProps) {
+export default function Home({ places }: MapProps) {
   return <HomeTemplate places={places} />
 }
 
 export const getStaticProps = async () => {
-  const {places} = await client.request<GetPlacesQuery>(GET_PLACES)
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES)
 
   return {
+    revalidate: 5,
     props: {
       places
     }
